be3416074c1d559a2f9cba7d97df0f583468c0b0
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-03-21 15:58:21 -0400

    Add lsp virtual_text block

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -12,6 +12,20 @@ lspconfig.clangd.setup {
   capabilities = capabilities,
 }
 
+vim.diagnostic.config({
+    underline = true,
+    signs = true,
+    virtual_text = false,
+    float = {
+        show_header = true,
+        source = 'if_many',
+        border = 'rounded',
+        focusable = false,
+    },
+    update_in_insert = false, -- default to false
+    severity_sort = false, -- default to false
+})
+
 local servers = {
   "pyright",
   "ruff_lsp",
