ea3fd49099d13227ecf5dbdc0b98856448d85afd
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-03-21 15:59:24 -0400

    Revert a bit

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -8,7 +8,8 @@ vim.keymap.set('n', '<leader>u', ':undo<CR>')
 vim.keymap.set('n', '<C-a>', [[:lua vim.fn.append('.', ',rudolph.pienaar@childrens.harvard.edu')<CR>]], { noremap = true, silent = true })
 vim.keymap.set('n', '<Leader>j', '<ESC>v4jgq' )
 vim.api.nvim_set_keymap('n', '<leader>at', ':ALEToggle<CR>', {noremap = true, silent = true})
-vim.keymap.set("n", "<A-m>", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "? toggles local troubleshoot" })
+
+vim.opt.clipboard = "unnamedplus"
 
 -- disable virtual_text (inline) diagnostics and use floating window
 -- format the message such that it shows source, message and
@@ -23,11 +24,22 @@ vim.diagnostic.config({
                 "%s (%s) [%s]",
                 diagnostic.message,
                 diagnostic.source,
-                diagnostic.code 
+                diagnostic.code or diagnostic.user_data.lsp.code
             )
         end,
     },
 })
+
+vim.api.nvim_set_keymap('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>d[', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>d]', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
+-- The following command requires plug-ins "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", and optionally "kyazdani42/nvim-web-devicons" for icon support
+vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
+-- If you don't want to use the telescope plug-in but still want to see all the errors/warnings, comment out the telescope line and uncomment this:
+-- vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>lua vim.diagnostic.setloclist()<CR>', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<space>e', ':Lspsaga show_line_diagnostics<CR>', { noremap = true, silent = true })
+
 
 M.dap = {
   plugin = true,
@@ -61,6 +73,5 @@ M.dap_python = {
     }
   }
 }
-
 
 return M
