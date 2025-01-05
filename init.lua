5ba8519fdc62ae7822d12ac74aa9165a57109c5c
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2025-01-05 11:49:35 -0500

    Add CopyDiagnostics

--- init.lua
+++ init.lua
@@ -29,4 +29,22 @@ vim.cmd('match ExtraWhitespace /\\s\\+$/')
 -- Define a command to strip trailing whitespace
 vim.cmd('command! -bar StripWhiteSpace %s/\\s\\+$//e')
 
+vim.api.nvim_create_user_command("CopyDiagnostics", function()
+    -- Get the current line number
+    local lnum = vim.api.nvim_win_get_cursor(0)[1] - 1
+    -- Retrieve diagnostics for the current line
+    local diagnostics = vim.diagnostic.get(0, { lnum = lnum })
 
+    -- Open a new vertical split buffer
+    vim.cmd("vnew")
+    -- Get the current buffer ID for the new buffer
+    local new_buf = vim.api.nvim_get_current_buf()
+
+    -- Loop through diagnostics and add messages to the new buffer
+    for _, diag in ipairs(diagnostics) do
+        -- Split the diagnostic message into individual lines
+        local lines = vim.split(diag.message, "\n", { plain = true })
+        -- Append each line to the buffer
+        vim.api.nvim_buf_set_lines(new_buf, -1, -1, false, lines)
+    end
+end, {})
